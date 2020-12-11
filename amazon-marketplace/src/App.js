ae17fb39a53230543cc8546153ec471c6d96ecb0
Vanessa Cattabiani <vanebrunocattabiani@gmail.com>
2020-12-11 12:09:24 +0100

    Add Product Page

--- amazon-marketplace/src/App.js
+++ amazon-marketplace/src/App.js
@@ -6,17 +6,21 @@ import NavBar from "./components/navBar";
 import MarketPlace from "./components/marketPlace/MarketPlace";
 import BackOffice from "./components/backOffice/BackOffice";
 import Footer from "./components/footer";
+import ProductPage from "./components/productPage/productPage";
 
 function App() {
   return (
     <div className='App'>
       <Router>
         <NavBar />
-        <Route path='/market'>
+        <Route path='/market' exact>
           <MarketPlace />
         </Route>
         <Route path='/backoffice' exact>
           <BackOffice />
+        </Route>
+        <Route path='/market/:id' exact>
+          <ProductPage />
         </Route>
         <Footer />
       </Router>
